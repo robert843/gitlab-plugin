35d00dcc3e94a46e2e2b90ad954d83e3df3cd48e
Robert Japełski <robert843@o2.pl>
2019-10-05 12:18:42 +0200

    add addGitLabRelease function

--- src/main/java/com/dabsquared/gitlabjenkins/gitlab/api/impl/V3GitLabApiProxy.java
+++ src/main/java/com/dabsquared/gitlabjenkins/gitlab/api/impl/V3GitLabApiProxy.java
@@ -4,17 +4,7 @@ package com.dabsquared.gitlabjenkins.gitlab.api.impl;
 import com.dabsquared.gitlabjenkins.gitlab.api.model.*;
 import com.dabsquared.gitlabjenkins.gitlab.hook.model.State;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.FormParam;
-import javax.ws.rs.GET;
-import javax.ws.rs.HEAD;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
 
@@ -221,4 +211,13 @@ interface V3GitLabApiProxy extends GitLabApiProxy {
     @Path("/projects/{projectId}/pipelines")
     @Override
     List<Pipeline> getPipelines(@PathParam("projectId") String projectId);
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/projects/{projectId}/releases")
+    @Override
+    default void addRelease(@PathParam("projectId") Integer projectId, @FormParam("name") String name, @FormParam("tag_name") String tag_name, @FormParam("description") String description){
+        throw new NotSupportedException();
+    }
+
 }
